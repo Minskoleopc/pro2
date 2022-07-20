1dd67bae8449cdcd7a08676f15294ffc5a243769
Minskoleopc <minskoleopc@gmail.com>
2022-07-20 21:11:21 +0530

    arrayDay-2

--- script.js
+++ script.js
@@ -36,102 +36,31 @@ const account4 = {
 
 const accounts = [account1, account2, account3, account4];
 
-// function to createUser name
-function createUserName(word){
-    let a = word.split(" ") // ["chinmay","deshpande"]
-    let b = a.map(function(el){  //['c','d']
-      return el[0]
-    })
-    let c = b.join("") //"cd"
-    return c
-}
-//createUserName("chinmay deshpande")
-accounts.forEach(function(el){
-    el.userName = createUserName(el.owner).toLowerCase()
-})
-console.log(accounts)
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
 
-// totalwithdrawl
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
 
-function calculateTotalWithDrawl(arr){
-    arr.forEach(function(el){
-      let a = el.movements.filter(function(el){
-         return el < 0
-      })
-      let b = a.reduce(function(acc,el){
-        return acc + el
-      },0)
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
 
-      el.totalDrawl = b
-
-    })
-}
-calculateTotalWithDrawl(accounts)
-console.log(accounts)
-
-// calculate total depoit()
-
-
-function calculateTotalDepoist(arr){
-  arr.forEach(function(el){
-    let a = el.movements.filter(function(el){
-       return el > 0
-    })
-    let b = a.reduce(function(acc,el){
-      return acc + el
-    },0)
-
-    el.totalDepoist = b
-
-  })
-}
-calculateTotalDepoist(accounts)
-console.log(accounts)
-// calculate current balance()
-
-function CurrentBalance(arr){
-  arr.forEach(function(el){
-    el.currentBal = el.totalDepoist - el.totalDrawl
-  })
-
-}
-CurrentBalance(accounts)
-console.log(accounts)
-
-
-
-
-
-
-
-
-
-
-// // Elements
-// const labelWelcome = document.querySelector('.welcome');
-// const labelDate = document.querySelector('.date');
-// const labelBalance = document.querySelector('.balance__value');
-// const labelSumIn = document.querySelector('.summary__value--in');
-// const labelSumOut = document.querySelector('.summary__value--out');
-// const labelSumInterest = document.querySelector('.summary__value--interest');
-// const labelTimer = document.querySelector('.timer');
-
-// const containerApp = document.querySelector('.app');
-// const containerMovements = document.querySelector('.movements');
-
-// const btnLogin = document.querySelector('.login__btn');
-// const btnTransfer = document.querySelector('.form__btn--transfer');
-// const btnLoan = document.querySelector('.form__btn--loan');
-// const btnClose = document.querySelector('.form__btn--close');
-// const btnSort = document.querySelector('.btn--sort');
-
-// const inputLoginUsername = document.querySelector('.login__input--user');
-// const inputLoginPin = document.querySelector('.login__input--pin');
-// const inputTransferTo = document.querySelector('.form__input--to');
-// const inputTransferAmount = document.querySelector('.form__input--amount');
-// const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-// const inputCloseUsername = document.querySelector('.form__input--user');
-// const inputClosePin = document.querySelector('.form__input--pin');
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
 
 // /////////////////////////////////////////////////
 // /////////////////////////////////////////////////
@@ -146,3 +75,160 @@ console.log(accounts)
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // /////////////////////////////////////////////////
+
+
+
+const createUserName = function(accs){
+  accs.forEach(function(acc){
+    acc.username = acc.owner // "chinmay deshpaade"
+    .split(' ') // ["chinmay" ,"deshpande"]
+    .map(function(name){
+      return name[0]  // ["c","d"]
+    }).join("").toLowerCase() // "cd"
+
+  })
+}
+createUserName(accounts)
+console.log(accounts)
+
+
+
+// Event handler one 
+
+btnLogin.addEventListener('click',function(e){
+  e.preventDefault()
+  let userInputUserName = inputLoginUsername.value
+  let userInputPin = inputLoginPin.value
+  let currentAccount = accounts.find(function(acc){
+    return acc.username == userInputUserName && acc.pin == userInputPin
+  })
+  if(currentAccount){
+    containerApp.style.opacity = "100"
+    inputLoginUsername.value = ""
+    inputLoginPin.value = ""
+    labelWelcome.textContent = `Welcome back ,${currentAccount.owner.split(' ')[0]}`
+
+  }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // logic
+
+// // function to createUser name
+// function createUserName(word){
+//   let a = word.split(" ") // ["chinmay","deshpande"]
+//   let b = a.map(function(el){  //['c','d']
+//     return el[0]
+//   })
+//   let c = b.join("") //"cd"
+//   return c
+// }
+// //createUserName("chinmay deshpande")
+// accounts.forEach(function(el){
+//   el.userName = createUserName(el.owner).toLowerCase()
+// })
+// console.log(accounts)
+
+// // totalwithdrawl
+
+// function calculateTotalWithDrawl(arr){
+//   arr.forEach(function(el){
+//     let a = el.movements.filter(function(el){
+//        return el < 0
+//     })
+//     let b = a.reduce(function(acc,el){
+//       return acc + el
+//     },0)
+
+//     el.totalDrawl = b
+
+//   })
+// }
+// calculateTotalWithDrawl(accounts)
+// console.log(accounts)
+
+// // calculate total depoit()
+
+
+// function calculateTotalDepoist(arr){
+// arr.forEach(function(el){
+//   let a = el.movements.filter(function(el){
+//      return el > 0
+//   })
+//   let b = a.reduce(function(acc,el){
+//     return acc + el
+//   },0)
+
+//   el.totalDepoist = b
+
+// })
+// }
+// calculateTotalDepoist(accounts)
+// console.log(accounts)
+// // calculate current balance()
+
+// function CurrentBalance(arr){
+// arr.forEach(function(el){
+//   el.currentBal = el.totalDepoist - el.totalDrawl
+// })
+
+// }
+// CurrentBalance(accounts)
+// console.log(accounts)
+
